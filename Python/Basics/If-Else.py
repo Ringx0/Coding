515ab6d9c604f7aca11a70b84862e3f3fe638b88
Ringx0 <100504303+Ringx0@users.noreply.github.com>
2023-08-09 13:12:08 +0100

    Update If-Else.py

--- Python/Basics/If-Else.py
+++ Python/Basics/If-Else.py
@@ -1,24 +1,19 @@
-#!/bin/python3
-
 import math
 import os
 import random
 import re
 import sys
 
-# The __name__ == '__main__' block is the main entry point of the script.
+n = int(input().strip())  # Read the input integer and store it in the variable 'n'
 
-if __name__ == '__main__':
-    n = int(input().strip())  # Read the input integer and store it in the variable 'n'
-
-    # Check if the integer 'n' is odd (not divisible by 2)
-    if (n % 2) != 0:
-        print("Weird")  # If 'n' is odd, print "Weird"
-    else:
-        # If 'n' is even (divisible by 2), check various ranges for the value of 'n'
-        if n >= 2 and n <= 5:
-            print("Not Weird")  # If 'n' is even and within the range [2, 5], print "Not Weird"
-        if n >= 6 and n <= 20:
-            print("Weird")  # If 'n' is even and within the range [6, 20], print "Weird"
-        if n > 20 and n <= 100:
-            print("Not Weird")  # If 'n' is even and within the range (20, 100], print "Not Weird"
+# Check if the integer 'n' is odd (not divisible by 2)
+if (n % 2) != 0:
+    print("Weird")  # If 'n' is odd, print "Weird"
+else:
+    # If 'n' is even (divisible by 2), check various ranges for the value of 'n'
+    if n >= 2 and n <= 5:
+        print("Not Weird")  # If 'n' is even and within the range [2, 5], print "Not Weird"
+    if n >= 6 and n <= 20:
+        print("Weird")  # If 'n' is even and within the range [6, 20], print "Weird"
+    if n > 20 and n <= 100:
+        print("Not Weird")  # If 'n' is even and within the range (20, 100], print "Not Weird"
