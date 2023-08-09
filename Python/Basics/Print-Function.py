993979748e76fbfd12a3ef48bbef57d5db0eb967
Ringx0 <100504303+Ringx0@users.noreply.github.com>
2023-08-09 11:39:05 +0100

    Update Print-Function.py

--- Python/Basics/Print-Function.py
+++ Python/Basics/Print-Function.py
@@ -1,11 +1,10 @@
-if __name__ == '__main__':
-    n = int(input())  # Read the integer 'n' from the user input and convert it to an integer.
-    
-    # Generate a list of integers from 1 to 'n' using a loop and convert each number to a string.
-    numbers = [str(i) for i in range(1, n + 1)]
-    
-    # Concatenate the numbers in the list without spaces, converting the list to a single string.
-    output = ''.join(numbers)
-    
-    # Print the final concatenated string.
-    print(output)
+n = int(input())  # Read the integer 'n' from the user input and convert it to an integer.
+
+# Generate a list of integers from 1 to 'n' using a loop and convert each number to a string.
+numbers = [str(i) for i in range(1, n + 1)]
+
+# Concatenate the numbers in the list without spaces, converting the list to a single string.
+output = ''.join(numbers)
+
+# Print the final concatenated string.
+print(output)
